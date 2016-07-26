55267104dec8ff98cee61b3ad71f289660e2984f
Varun Hasija <witty123@Varuns-MacBook-Pro.local>
2016-07-26 16:37:25 +0530

    added basic files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='wikionterm',
+    version='0.1',
+    py_modules=['wikionterm'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        wikionterm=wikionterm:cli
+    ''',
+)
