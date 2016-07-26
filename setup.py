2fd74f696deb58d8189066ea9c1a0e073fc946e2
Varun Hasija <witty123@Varuns-MacBook-Pro.local>
2016-07-27 00:56:44 +0530

    url printed

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='wikionterm',
-    version='0.1',
+    version='0.1.1',
     py_modules=['wikionterm'],
     install_requires=[
         'Click',
     ],
     entry_points='''
         [console_scripts]
-        wikionterm=wikionterm:cli
+        wikionterm=wikionterm:search
     ''',
 )
--- wikionterm.py
+++ wikionterm.py
@@ -1,6 +1,15 @@
 import click
+import urllib2
+import json
 
 @click.command()
-def cli():
-    """Example script."""
-    click.echo('Hello World!')
+@click.argument('search_term')
+def search(search_term):
+	 jsonres =  urllib2.urlopen("https://en.wikipedia.org/w/api.php?action=opensearch&search="+ search_term +"&limit=1&namespace=0&format=json").read()
+	 jsonres = str(jsonres)
+	 jsonres = jsonres.split(",")
+	 #beg = jsonres.find("http")
+	 url = jsonres[3]
+	 click.echo(str(url))
+	 
+
