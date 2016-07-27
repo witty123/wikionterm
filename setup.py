131102e37cd454dc15ec4b7d34ea968a2d234dda
Varun Hasija <witty123@Varuns-MacBook-Pro.local>
2016-07-27 17:40:05 +0530

    add file access

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='wikionterm',
     version='0.1.1',
-    py_modules=['wikionterm'],
+    packages=find_packages(),
+    include_package_data=True,
+    #py_modules=['wikionterm'],
     install_requires=[
         'Click',
     ],
--- wikionterm.py
+++ wikionterm.py
@@ -3,13 +3,20 @@ import urllib2
 import json
 
 @click.command()
+@click.option('output', type=click.File('wb'))
 @click.argument('search_term')
 def search(search_term):
 	 jsonres =  urllib2.urlopen("https://en.wikipedia.org/w/api.php?action=opensearch&search="+ search_term +"&limit=1&namespace=0&format=json").read()
 	 jsonres = str(jsonres)
 	 jsonres = jsonres.split(",")
-	 #beg = jsonres.find("http")
 	 url = jsonres[3]
+	 url = url.replace("[","")
+	 url = url.replace("]","")
+	 url = url.replace("\"","")
 	 click.echo(str(url))
 	 
+
+
+
+
 
