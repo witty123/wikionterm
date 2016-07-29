46b88e41399904a4182cc72252010df967e08cfa
Devang Bacharwar <devanggtgr8@gmail.com>
2016-07-29 15:58:13 +0530

    Interactive done

--- wikionterm.py
+++ wikionterm.py
@@ -1,14 +1,22 @@
 import click
 import urllib2
+import urllib
 import json
 
 @click.command()
-@click.argument('log', type=click.File('a+'), help="URL log file")
-@click.argument('search_term', help="Word you want to search wkipedia for")
-def search(search_term, out):
+@click.option('--log', type=click.File('a'), help="URL log file")
+@click.option('--search_term', help="Word you want to search wkipedia for")
+def search(search_term,log):
+    if log == None:
+        click.echo("Enter the log file")
+        log = raw_input()
+        log = open(log, 'a')
+    if search_term == None:
+        click.echo("Enter the search term")
+        search_term = raw_input()
     jsonres =  urllib2.urlopen("https://en.wikipedia.org/w/api.php?action=opensearch&search="+ search_term +"&limit=1&namespace=0&format=json").read()
     jsonres = str(jsonres)
-    click.echo(jsonres)
+    # click.echo(jsonres)
     jsonres = jsonres.split(",")
     url = jsonres[len(jsonres)-1]
     url = url.replace("[","")
