3076dae20120edc157b3113131a4e5c7b258655e
Devang Bacharwar <devanggtgr8@gmail.com>
2016-07-29 14:59:00 +0530

    Link extractction debug

--- wikionterm.py
+++ wikionterm.py
@@ -3,20 +3,20 @@ import urllib2
 import json
 
 @click.command()
-@click.option('output', type=click.File('wb'))
-@click.argument('search_term')
-def search(search_term):
-	 jsonres =  urllib2.urlopen("https://en.wikipedia.org/w/api.php?action=opensearch&search="+ search_term +"&limit=1&namespace=0&format=json").read()
-	 jsonres = str(jsonres)
-	 jsonres = jsonres.split(",")
-	 url = jsonres[3]
-	 url = url.replace("[","")
-	 url = url.replace("]","")
-	 url = url.replace("\"","")
-	 click.echo(str(url))
-	 
-
-
-
-
-
+@click.argument('log', type=click.File('a+'), help="URL log file")
+@click.argument('search_term', help="Word you want to search wkipedia for")
+def search(search_term, out):
+    jsonres =  urllib2.urlopen("https://en.wikipedia.org/w/api.php?action=opensearch&search="+ search_term +"&limit=1&namespace=0&format=json").read()
+    jsonres = str(jsonres)
+    click.echo(jsonres)
+    jsonres = jsonres.split(",")
+    url = jsonres[len(jsonres)-1]
+    url = url.replace("[","")
+    url = url.replace("]","")
+    url = url.replace("\"","")
+    url = str(url)
+    if url != "":
+        click.echo(url+'\n')
+        log.write(url+'\n')
+    else:
+        click.echo("No wiki link found!")
