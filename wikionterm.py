c9e75ca12690db0f43c4cf0539a00906baa0a52e
Devang Bacharwar <devanggtgr8@gmail.com>
2016-07-29 16:24:36 +0530

    Link done

--- wikionterm.py
+++ wikionterm.py
@@ -5,16 +5,16 @@ import json
 
 @click.command()
 @click.option('--log', type=click.File('a'), help="URL log file")
-@click.option('--search_term', help="Word you want to search wkipedia for")
-def search(search_term,log):
+@click.option('--search', help="Word you want to search wkipedia for")
+def search(search,log):
     if log == None:
         click.echo("Enter the log file")
         log = raw_input()
         log = open(log, 'a')
-    if search_term == None:
+    if search == None:
         click.echo("Enter the search term")
-        search_term = raw_input()
-    jsonres =  urllib2.urlopen("https://en.wikipedia.org/w/api.php?action=opensearch&search="+ urllib.quote(search_term) +"&limit=1&namespace=0&format=json").read()
+        search = raw_input()
+    jsonres =  urllib2.urlopen("https://en.wikipedia.org/w/api.php?action=opensearch&search="+ urllib.quote(search) +"&limit=1&namespace=0&format=json").read()
     jsonres = str(jsonres)
     # click.echo(jsonres)
     jsonres = jsonres.split(",")
